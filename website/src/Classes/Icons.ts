0cce98aa77ba63cd960df9f47a002805601ce6a7
Tassilo <tassia710@gmail.com>
2021-04-04 20:22:53 +0200

    Update Icons.ts

--- website/src/Classes/Icons.ts
+++ website/src/Classes/Icons.ts
@@ -11,4 +11,27 @@ export class Icons {
 	public static readonly GITHUB = "/img/social/github.svg";
 	public static readonly GITLAB = "/img/social/gitlab.svg";
 
+	public static readonly ANGULAR = "/img/language/angular.svg";
+	public static readonly C = "/img/language/c.svg";
+	public static readonly CPP = "/img/language/cpp.svg";
+	public static readonly CSHARP = "/img/language/csharp.svg";
+	public static readonly CSS3 = "/img/language/css3.svg";
+	public static readonly FLUTTER = "/img/language/flutter.svg";
+	public static readonly GOLANG = "/img/language/golang.svg";
+	public static readonly GRAPHQL = "/img/language/graphql.svg";
+	public static readonly HASKELL = "/img/language/haskell.svg";
+	public static readonly HTML5 = "/img/language/html5.svg";
+	public static readonly JAVA = "/img/language/java.svg";
+	public static readonly JAVASCRIPT = "/img/language/javascript.svg";
+	public static readonly KOTLIN = "/img/language/kotlin.svg";
+	public static readonly PHP = "/img/language/php.svg";
+	public static readonly POWERSHELL = "/img/language/powershell.svg";
+	public static readonly PYTHON = "/img/language/python.svg";
+	public static readonly REACT = "/img/language/react.svg";
+	public static readonly RUBY = "/img/language/ruby.svg";
+	public static readonly SASS = "/img/language/sass.svg";
+	public static readonly SPRING = "/img/language/spring.svg";
+	public static readonly SWIFT = "/img/language/swift.svg";
+	public static readonly TYPESCRIPT = "/img/language/typescript.svg";
+
 }
